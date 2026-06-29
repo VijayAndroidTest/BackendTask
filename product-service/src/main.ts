7f85ad549cd474346fc4c6389612d4d7315e6b8c
Vijay <vijayarajan1003@gmail.com>
2026-06-29 19:26:40 +0530

    Add missing all

--- product-service/src/main.ts
+++ product-service/src/main.ts
@@ -7,12 +7,10 @@ async function bootstrap() {
 
   app.enableCors({ origin: '*', methods: 'GET,POST,PUT,DELETE' });
 
-  // REMOVE the app.connectMicroservice and app.startAllMicroservices()
-  // They are not needed if you are using pure REST/HTTP.
-
   const port = process.env.PORT || 4001;
   await app.listen(port, '0.0.0.0');
   console.log(`Product Service running on port: ${port}`);
 }
-bootstrap();
+
+// Ensure this is only called once!
 bootstrap();
